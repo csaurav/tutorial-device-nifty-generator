78c100ac67692e00dae38a93e2cb1ca23595db16
Saurav <csaurav80@gmail.com>
2012-07-31 23:09:27 +0530

    Project base directory

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ideaz::Application.config.secret_token = '3d6acb502ecd4a976e3c981233187ff924e01042d7c08a6aa3c98ea99007cce25c2ce52e62751f8667d6218ac746d469edd065e4058c08e9bfbf39d98ec63607'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ideaz::Application.config.session_store :cookie_store, key: '_ideaz_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ideaz::Application.config.session_store :active_record_store

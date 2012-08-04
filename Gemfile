83492d337ed357a6671b7db6442dd4803938f488
Saurav <csaurav80@gmail.com>
2012-08-04 11:00:14 +0530

    Added nifty generator gem and have done the necessary configurations for using it

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ group :assets do
 end
 
 gem 'jquery-rails'
+gem "nifty-generators", :group => :development
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Ideaz::Application.routes.draw do
+  get "destroy/test"
+
   get "welcome/index"
 
   # The priority is based upon order of creation:
@@ -51,6 +53,7 @@ Ideaz::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'welcome#index'
+  resources :test
 
   # See how all your routes lay out with "rake routes"
 
